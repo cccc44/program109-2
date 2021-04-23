c208fbff909c8b2ff282f4d90cf3f1139d1da76f
cccc44 <79676985+cccc44@users.noreply.github.com>
2021-04-23 11:09:17 +0800

    Add files via upload

---
+++ week08-one.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char tree[1000000][32];
+int compare(const void * p1,const void * p2)
+{
+	return strcmp( (char*) p1,(char*) p2 );
+}
+int main()
+{
+	int n;
+	scanf("%d\n\n",&n);
+	int m=0;
+	for(int i=0; ;i++){
+		char* now=gets( tree[i] );
+		if( now == NULL ){
+            m=i;
+            break;
+		}
+		if( strcmp( tree[i],"")==0){
+            m=i;
+            break;
+		}
+	}
+	qsort( tree, m, 32, compare );
+	float ans=1;
+	for(int i=0;i<m;i++){
+		if(strcmp(tree[i],tree[i+1])!=0){
+			printf("%s %.4f\n",tree[i],100*ans/(float)m);
+			ans=1;
+		}
+		else{
+			ans++;
+		}
+	}
+}
